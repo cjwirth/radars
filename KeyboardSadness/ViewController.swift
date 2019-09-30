0617ba46ca9332c4455fabbb515d4e1b2775319a
Caesar Wirth <caesar@cjwirth.com>
2019-09-30 13:50:47 -0400

    Add keyboard switching code that fails

--- KeyboardSadness/ViewController.swift
+++ KeyboardSadness/ViewController.swift
@@ -8,13 +8,65 @@
 
 import UIKit
 
+class KeyboardSwitchingTextField: UITextField {
+
+    override var textInputMode: UITextInputMode? {
+        // Let's force a German keyboard if one exists
+        if let german = UITextInputMode.activeInputModes.first(where:  { $0.primaryLanguage?.hasPrefix("de-") ?? false }) {
+            return german
+        }
+        return super.textInputMode
+    }
+
+}
+
+class KeyboardSwitchingTextView: UITextView {
+
+    override var textInputMode: UITextInputMode? {
+        // Let's force a German keyboard if one exists
+        if let german = UITextInputMode.activeInputModes.first(where:  { $0.primaryLanguage?.hasPrefix("de-") ?? false }) {
+            return german
+        }
+        return super.textInputMode
+    }
+
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+
+        let textView = KeyboardSwitchingTextView()
+        view.addSubview(textView)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            textView.topAnchor.constraint(equalTo: view.topAnchor),
+            textView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            textView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            textView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25)
+        ])
+
+        let textField = KeyboardSwitchingTextField()
+        textField.backgroundColor = .red
+        view.addSubview(textField)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: textView.bottomAnchor, constant: 10),
+            textField.leftAnchor.constraint(equalTo: view.leftAnchor),
+            textField.rightAnchor.constraint(equalTo: view.rightAnchor),
+            textField.heightAnchor.constraint(equalToConstant: 44)
+            ])
+
+        let gr = UITapGestureRecognizer(target: self, action: #selector(finishEditing))
+        view.addGestureRecognizer(gr)
+
     }
 
+    @objc func finishEditing() {
+        view.endEditing(true)
+    }
 
 }
 
